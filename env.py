c2a54d376a967cb7769ddcfd2e06171eca09c26d
MS-DZULQURNAIN <121719789+MS-DZULQURNAIN@users.noreply.github.com>
2023-04-01 19:26:21 +0700

    Update env.py

--- env.py
+++ env.py
@@ -6,25 +6,25 @@ load_dotenv()
 API_ID = os.getenv("API_ID", "").strip()
 API_HASH = os.getenv("API_HASH", "").strip()
 BOT_TOKEN = os.getenv("BOT_TOKEN", "").strip()
-DATABASE_URL = os.getenv("DATABASE_URL", "").strip() # Not a necessary variable anymore but you can add to get stats
+DATABASE_URL = os.getenv("DATABASE_URL", "").strip()
 MUST_JOIN = os.getenv("MUST_JOIN", "")
 
 if not API_ID:
-    raise SystemExit("No API_ID found. Exiting...")
+    raise SystemExit("API_ID Tidak tersedia. Keluar...")
 elif not API_HASH:
-    raise SystemExit("No API_HASH found. Exiting...")
+    raise SystemExit("API_HASH Tidak tersedia. Keluar...")
 elif not BOT_TOKEN:
-    raise SystemExit("No BOT_TOKEN found. Exiting...")
+    raise SystemExit("BOT_TOKEN Tidak tersedia. Keluar...")
 '''
 if not DATABASE_URL:
-    print("No DATABASE_URL found. Exiting...")
+    print("DATABASE_URL Tidak tersedia. Keluar...")
     raise SystemExit
 '''
 
 try:
     API_ID = int(API_ID)
 except ValueError:
-    raise SystemExit("API_ID is not a valid integer. Exiting...")
+    raise SystemExit("API_ID Tidak cocok. Keluar...")
 
 if 'postgres' in DATABASE_URL and 'postgresql' not in DATABASE_URL:
     DATABASE_URL = DATABASE_URL.replace("postgres", "postgresql")
